462089683866c08b9f2d886ab0a907d2b865a892
jeevan thapa <jthapa01@outlook.com>
2022-11-01 11:04:17 -0400

    Updated values.yaml for sevice settings.

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -54,7 +54,7 @@ namespace Play.Inventory.Service
             });
             services.AddHealthChecks()
                 .AddMongoDb();
-                services.AddSeqLogging(Configuration);
+            services.AddSeqLogging(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
